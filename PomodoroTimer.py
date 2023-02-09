c30a40dd12aca3dfeaee540ca0956cf2ed35ea25
keitosuwahara <210103gi@yse-c.net>
2023-02-10 08:51:11 +0900

    up

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -1,28 +1,6 @@
-from tkinter import ttk
-from tkinter import messagebox
-import tkinter as tk
-
-#configure root main window
-root = tk.Tk()
-root.title("PomodoroTimer")
-root.geometry("600x400")
+import PySimpleGUI as sg
+import time
 
 
-#place and make a main frame
-frame = tk.Frame(root)
-frame.pack()
-
-
-#make various widgets
-btn_ttk = ttk.Button(frame, text="PomodoroStart", command=print("sss"))
-
-main_label = ttk.Label(frame, text="okokok", font=("MSゴシック", 30, "bold"))
-
-
-#place various widgets
-btn_ttk.grid(row=1, column=0, pady=5)
-main_label.grid(row=0, column=0)
-
-root.mainloop()
 
 
